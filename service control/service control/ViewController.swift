d271150ae4f92d0a71469b3618fd92dff9211dcd
NitinRajj <jathin.here@gmai.com>
2019-04-09 13:47:18 +0530

    some chelates=

---
+++ service control/service control/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  service control
+//
+//  Created by jathinraj on 09/04/19.
+//  Copyright © 2019 jathinraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        ///lanja
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
